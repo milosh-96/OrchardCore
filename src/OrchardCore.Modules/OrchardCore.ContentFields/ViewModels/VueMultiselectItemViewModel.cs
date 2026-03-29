7ce45051e1d745b1aad01c9f82b49dd9f1d30120
Miloš Jovanović <milos.jovanovic@lombiq.com>
2026-03-29 04:35:28 +0200

    Adding culture to the content picker option.

--- src/OrchardCore.Modules/OrchardCore.ContentFields/ViewModels/VueMultiselectItemViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.ContentFields/ViewModels/VueMultiselectItemViewModel.cs
@@ -5,6 +5,7 @@ public class VueMultiselectItemViewModel
     public string Id { get; set; }
     public string DisplayText { get; set; }
     public bool HasPublished { get; set; }
+    public string Culture { get; set; }
     public bool IsViewable { get; set; }
     public bool IsEditable { get; set; }
     public bool IsClickable => IsEditable || IsViewable;
